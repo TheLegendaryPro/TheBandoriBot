4b916946baac6da886ffd0fd1bb4596469730654
Nameless <nameless@monster.com>
2020-09-03 17:51:08 +0800

    updated database, ready to launch

--- update_mongo.py
+++ update_mongo.py
@@ -10,15 +10,18 @@ with open('bot_config/secrets.json', 'r') as f:
 
 mongo = motor.motor_asyncio.AsyncIOMotorClient(str(connection_url))
 db = mongo["TheBandoriBot"]
-config = Document(db, "server_config")
+config = Document(db, "user_db")
 
-with open('bot_config/prefixes.json') as f:
-    prefix_data = json.load(f)
+with open('bot_data/user_db.json') as f:
+    user_db = json.load(f)
 
 
 async def main():
-    for key, value in prefix_data.items():
-        await config.upsert({"_id": int(key), "prefix": value})
+    for value in user_db["_default"].items():
+        data = value[1]
+        data['_id'] = data.pop('user_id')
+        print(data)
+        await config.upsert(data)
 
 
 newfeature = asyncio.get_event_loop().run_until_complete(main())
